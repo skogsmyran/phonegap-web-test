79da4b6bad14a54673e2d86fb1a1aebcb6b8c425
Pontus Wiberg @ SE-313 <pontus@pontusw.com>
2013-10-29 11:18:48 +0100

    changed css and function routing

--- js/gamescreen.js
+++ js/gamescreen.js
@@ -1,8 +1,12 @@
 var Gamescreen = function() {
 	
 	this.initialize = function() {
+		var self = this;
 		this.el = $('<div/>');
-		this.el.on("click", "#play1", this.launchgame);
+		this.el.on("click", ".game", function() {
+			var id = this.id;
+			self.launchgame(id);
+		});
 	},
 
 	this.render = function() {
@@ -10,8 +14,13 @@ var Gamescreen = function() {
         return this;
 	},
 
-	this.launchgame = function() {
-		window.location.hash = "#charades";
+	this.launchgame = function(game) {
+		if (game == "g1") {
+			window.location.hash = "#charades";
+		} else {
+			alert("Sorry, you can't play this right now :(");  
+ 			return false; 
+		}
 	},
 
 	this.initialize();
