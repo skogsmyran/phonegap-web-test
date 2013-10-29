cabdc19f7e77ca9d5ed370e809decca63b51fa55
Pontus Wiberg @ SE-313 <pontus@pontusw.com>
2013-10-29 18:31:13 +0100

    changed to remove/attach instead of hide/show in jquery, not working properly yet..

--- js/gamecharades._bjs.js
+++ js/gamecharades._bjs.js
@@ -18,17 +18,12 @@ var Gamecharades = function() {
 		});
 	},
 
-	this.hideshow = function(item,status) {
-		if (status == "hide") {
-			var item = item.detach();
-		}
-	}
-
 	this.endround = function(round, rounds, t1score, t2score) {
 		var self = this;
-		$('#charadetodo').remove();
-		$('#Timer').remove();
-		$('.gamewrapper').append(teambuttons);
+		$('#team1').css("visibility", "visible");
+		$('#team2').css("visibility", "visible");
+		$('#charadetodo').css("visibility","hidden");
+		$('#Timer').css("visibility", "hidden");
 		var charadeinstructions = "Who won this round?"
 		document.getElementById('charadeinstructions').innerHTML = charadeinstructions;
 		this.el.on("click", "#team1", function(round,rounds,t1score,t2score) {
@@ -45,15 +40,19 @@ var Gamecharades = function() {
 		});
 	}
 
+	this.pregame = function() {
+		
+	}
+
 	this.endgame = function(t1score,t2score) {
 		console.log("endgame");
 	}
 
 	this.startround = function(round, rounds, t1score, t2score) {
 		var self = this;
-		$('#team1').remove();
-		$('#team2').remove();
-		$('.gamewrapper').append(charadetodo);
+		$('#team1').css("visibility", "hidden");
+		$('#team2').css("visibility", "hidden");
+		$('#charadetodo').css("visibility","visible");
 		if (round == rounds) {
 			self.endgame(t1score,t2score);
 		}
@@ -61,12 +60,13 @@ var Gamecharades = function() {
 		document.getElementById('charadeinstructions').innerHTML = charadeinstructions;
 		var charadetodisplay = self.getcharade();
 		document.getElementById('charadetodo').innerHTML = charadetodisplay;
+		$('#Timer').css("visibility", "visible");
 		self.timer(round, rounds, t1score, t2score);
 	}
 
 	this.game = function(rounds) {
 		var self = this;
-		$('#startcharades').detach();
+		$('#startcharades').remove();
 		var rounds = rounds;
 		var t1score = 0;
 		var t2score = 0;
@@ -94,18 +94,10 @@ var Gamecharades = function() {
         return charadesarray[generatedid];
 	},
 
-	
-
 	this.initialize();
 };
 
 var gamecharadeshtml = $("#gamecharades").html();
 Gamecharades.template = Handlebars.compile(gamecharadeshtml);
-
-var teambuttonshtml = $("#teambuttons").html();
-var teambuttons = Handlebars.compile(teambuttonshtml);
-
-var charadetodohtml = $("#charadetodo").html();
-var charadetodo = Handlebars.compile(charadetodohtml);
 
 var charadesarray = ["Wings","Guitar","Cow","Summer","Elbow","Rope","Ball","Rollerblade","Fang","Snowball","Turtle","Ear","Cheek","Smile","Jar","Tail","Basketball","Mouth","Telephone","Star","Tree","Airplane","Point","Alarm","Table tennis","Beg","Drum","Cape","Chin","Roof","Rain","Saddle","Room"]
